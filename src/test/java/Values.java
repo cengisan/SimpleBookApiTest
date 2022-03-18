3f0cedac76ed5a51f34ccfc9e7894cbc05df1540
cengisan <77883086+cengisan@users.noreply.github.com>
2022-03-18 21:41:04 +0300

    Create Values.java

---
+++ src/test/java/Values.java
@@ -0,0 +1,21 @@
+public class Values {
+
+    private String AuthToken;
+    private String id;
+
+    public void setAuthToken(String AuthToken){
+        this.AuthToken = AuthToken;
+    }
+
+    public String getAuthToken(){
+        return AuthToken;
+    }
+
+    public void setOrderId(String id){
+        this.id = id;
+    }
+
+    public String getOrderId(){
+        return id;
+    }
+}
